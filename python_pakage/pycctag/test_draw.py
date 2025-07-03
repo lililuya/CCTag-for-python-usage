9ae75e27a5741e0bcdf23e967d5ce4d3ac28d989
WenLi <141640497+lililuya@users.noreply.github.com>
2025-07-03 23:41:49 +0800

    fix: hiden absolute path

--- python_pakage/pycctag/test_draw.py
+++ python_pakage/pycctag/test_draw.py
@@ -1,10 +1,10 @@
 import cv2
 import sys
-sys.path.append("/mnt/hd1/cxh/liwen/CCTag-develop/python_pakage/pycctag")
+sys.path.append("./CCTag-for-python-usage/python_pakage/pycctag")
 import pycctag
 
-markervector = pycctag.detect_from_file("/mnt/hd1/cxh/liwen/CCTag-develop/sample/01.png")
-image = cv2.imread("/mnt/hd1/cxh/liwen/CCTag-develop/sample/01.png")
+markervector = pycctag.detect_from_file("./CCTag-for-python-usage/sample/01.png")
+image = cv2.imread("./CCTag-for-python-usage/sample/01.png")
 markerlist = list(markervector)
 for i in range(len(markerlist)):
     print(str(markerlist[i].id)+"   "+str(markerlist[i].status))
@@ -12,4 +12,4 @@ for i in range(len(markerlist)):
     pointx = int(markerlist[i].x)
     pointy = int(markerlist[i].y)
     cv2.circle(image, (pointx, pointy), radius=10, color=(0, 255, 0), thickness=-1)
-cv2.imwrite("/mnt/hd1/cxh/liwen/CCTag-develop/sample/01_result.png", image)
+cv2.imwrite("./CCTag-for-python-usage/sample/01_result.png", image)
